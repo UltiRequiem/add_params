ba51217361c007b728e1af61e164590aaedf9f1e
GitHub <noreply@github.com>
2022-09-01 00:21:16 +0000

    ⬆️  Update dependencies

--- test.ts
+++ test.ts
@@ -1,7 +1,7 @@
 import {
   assertEquals,
   assertInstanceOf,
-} from "https://deno.land/std@0.150.0/testing/asserts.ts";
+} from "https://deno.land/std@0.153.0/testing/asserts.ts";
 
 import { addParams } from "./mod.ts";
 
